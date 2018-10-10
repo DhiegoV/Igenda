b718cd4bc277bfe451e74864514ff4c90fd8f2d8
dhiegov <dhiegovinicius1@gmail.com>
2018-10-10 10:56:59 -0300

    documentando métodos de Fachada

--- Fachada.py
+++ Fachada.py
@@ -5,11 +5,20 @@ class Fachada:
 		self.atividades = []
 
 	def apagar_atividade(self, atividade):
+		"""Dada uma atividade, apague-a do sistema.
+
+		`atividade` eh uma instancia de Atividade.
+		"""
 		self.atividades.remove(atividade)
 
 	def criar_atividade(self, atividade):
+		"""Dada uma atividade, adicione-a ao sistema.
+
+		`atividade` eh uma instancia de Atividade.
+		"""
 		self.atividades.append(atividade)
 
 	def obter_atividades(self):
+		"""Retorne uma lista com as atividades do sistema."""
 		return self.atividades
 
